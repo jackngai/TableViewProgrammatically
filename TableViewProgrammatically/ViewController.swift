c38920126d93a23d12ee198ff4494f784d31218c
Jack Ngai <jackngai@gmail.com>
2017-05-23 13:04:58 -0400

    feat: Added 5 sample rows
    Added 5 sample rows and programmatically setup the cells
    with a label called "Sample Item" and constraint that label
    using constraintsWithVisualFormat.

--- TableViewProgrammatically/ViewController.swift
+++ TableViewProgrammatically/ViewController.swift
@@ -12,9 +12,47 @@ class MyTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        navigationItem.title = "My TableView"
+        
+        tableView.register(MyCell.self, forCellReuseIdentifier: "cellID")
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+    }
 }
 
+class MyCell: UITableViewCell {
+    
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sample Item"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    func setupViews(){
+        
+        addSubview(nameLabel)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        
+    }
+    
+}
